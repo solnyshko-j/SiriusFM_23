e68de7154742095c74c6af2f0936dbfe299f15dc
Yulia <ersovau16@gmail.com>
2021-02-06 10:08:28 +0300

    Add some files

---
+++ Diffusion_OU.h
@@ -0,0 +1,21 @@
+#pragma once
+amespace SiriusFM{
+class DiffusionOU{
+private:
+  double const m_sigma;
+  double const m_kappa;
+  double const m_theta;
+
+public:
+  DiffusionOU(double a_kappa,double a_theta, double a_sigma):m_kappa(a_kappa),m_theta(a_theta),m_sigma(a_sigma){
+    if(m_sigma <= 0) throw std::invalid_argument("invalid sigma");
+    if(m_kappa <= 0) throw std::invalid_argument("invalid kappa");
+  }
+  double mu(double a_s, double a_t){
+    return m_kappa*(m_theta-a_s);
+  }
+  double sigma(double a_s, double a_t){
+    return (a_s < 0 ? 0 : m_sigma*a_s);
+  }
+};
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Diffusion_GBM.h"
+#include "Diffusion_Lipton.h"
+#include "Diffusion_CEV.h"
+#include "Diffusion_CIR.h"
+#include "Diffusion_OU.h"
+int main(){
+  std::cout<<"ura";
+  SiriusFM::DiffusionGBM(-2,-2);
+}
